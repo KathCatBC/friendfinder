e3412d91250fe6284ca03a3f9b98c8a371812601
Kathleen Catlett <kathleencatlett@comcast.net>
2017-02-21 17:17:09 -0500

    pseudo code

---
+++ server.js
@@ -0,0 +1,41 @@
+Start pseudo code
+
+look at star wars code
+handlebars??
+
+Load & Require npm packages
+	express (loaded)
+	path	(loaded)
+	body-parser  (loaded)
+
+Create landing (home page)
+	Title
+	link to survey page
+
+Create Survey page
+	Just start with empty questions 
+	have drop down or radio buttons 1-5
+	name text box
+	photo box
+	Load data into datafile
+	Do Routes
+		post
+			survey results
+		get
+			survey
+			results
+
+		use
+			homepage
+
+
+	do calcs
+	find match
+	ask about the math 
+		since (a1-b1)+(a2-b2)+(a3-b3)+(a4-b4)...  = (a1+a2+a3+a4)-(b1+b2+b3+b4)
+		can't I just add the number then look for the closest match
+
+
+
+
+
