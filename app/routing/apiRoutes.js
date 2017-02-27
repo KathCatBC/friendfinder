6d0f167a0572c11b13cbc8f0bdd22bdeace65cdb
Kathleen Catlett <kathleencatlett@comcast.net>
2017-02-27 14:54:13 -0500

    almost done

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -14,42 +14,29 @@ router.get("/friends", function(req,res){
 
 
 router.post("/friends", function(req, res) {
-
-
-	
-	console.log(req.body);
 	
 	var addedFriend = req.body
 	var newScores = addedFriend.scores
 
-	console.log("added Friend scores = " + addedFriend.scores)
+	var bestMatch = friends[0];
+	var bestScore = 100;  // set the bar higher than the best possible score, since lowest score is best match.
 
+	for (i=0; i<=friends.length-1; i++) {
+		match = friends[i];
+		currentScore = 0;
+		for (j=0; j<=9; j++){
+			currentScore = parseInt(currentScore) + Math.abs(parseInt(newScores[j]) - parseInt(match.scores[j]));
+			// I don't know why I needed parseInt, but it was the only way to make it work.
+		}
 
-var bestMatch = friends[0];
-var bestScore = 100;  // set the bar higher than the best possible score
-for (i=0; i<=friends.length-1; i++) {
-	console.log("i = " + i);
-	match = friends[i];
-	currentScore = 0;
-	for (j=0; j<=9; j++){
-		console.log("J = "+ newScores[j] + "  " + match.scores[j]);
-		console.log("J subtract & abs = "+ Math.abs(parseInt(newScores[j]) - parseInt(match.scores[j]))); 
-		currentScore = parseInt(currentScore) + Math.abs(parseInt(newScores[j]) - parseInt(match.scores[j]));
-		console.log("currentScore = " + parseInt(currentScore));
+		if (currentScore < bestScore) {
+			bestScore = currentScore;
+			bestMatch = friends[i];
+		}
 	}
-	console.log("currentScore = "+ currentScore)
-	if (currentScore < bestScore) {
-		bestScore = currentScore;
-		bestMatch = friends[i];
-		console.log(bestMatch.name)
-	}
-}
 
-// pop up mod with match
-console.log("best match = " + bestMatch.name)
+	friends.push(req.body);
 	res.json(bestMatch);
-
-  friends.push(req.body);
 });
 
 module.exports = router
